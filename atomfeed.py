55b8369d410e19e1fff471baa7fadf5d87622095
Marek Gibney <2785829+no-gravity@users.noreply.github.com>
2025-01-18 10:00:13 +0100

    Create atomfeed.py

---
+++ atomfeed.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+# The xml must start right after the """, or Firefox will not accept it due to leading whitespace
+atom_template = """<?xml version="1.0" encoding="utf-8"?>
+    <feed xmlns="http://www.w3.org/2005/Atom">
+        <title>{title}</title>
+        <id>{id}</id>
+        <subtitle>{subtitle}</subtitle>
+        <link href="{link_web}"/>
+        <link href="{link_feed}" rel="self"/>
+        <updated>{updated}</updated>
+        <author>
+            <name>{author_name}</name>
+            <email>{author_mail}</email>
+        </author>
+        {entries}
+    </feed>"""
+
+entry_template = """
+    <entry>
+        <title>{title}</title>
+        <link href='{link}'/>
+        <id>{link}</id>
+        <updated>{updated}</updated>
+        <summary>{summary}</summary>
+        <content type="html">
+            {content}
+        </content>
+    </entry>
+
+"""
+
+def template_dict(template, data):
+    r = template;
+    for tag in data.keys():
+        r = r.replace('{'+tag+'}',data[tag])
+    return r
+
+def generate(data):
+    feed = ""
+    most_recent = "0000-00-00"
+    for entry in data["entries"]:
+        most_recent = max(most_recent, entry["updated"])
+        feed += template_dict(entry_template, entry)
+    data["entries"] = feed;
+    # If no feed update time was provided, use the one of the most recent entry:
+    if not data["updated"]: data["updated"] = most_recent
+    return template_dict(atom_template, data)
